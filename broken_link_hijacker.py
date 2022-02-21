c6b23a42b6774a2ce8efd4effc83413558244b08
Sahil Dari <52995565+sahildari@users.noreply.github.com>
2022-02-21 16:23:39 +0530

    Update broken_link_hijacker.py

--- broken_link_hijacker.py
+++ broken_link_hijacker.py
@@ -3,18 +3,19 @@
 import requests
 import os
 from bs4 import BeautifulSoup
- 
-def brokenLinkHijack(Find_links,all_links,Find_links_present):
+
+def brokenLinkHijack(Find_links, all_links, Find_links_present):
     for Find_link in Find_links:
         for link in all_links:
             if Find_link in link.attrs['href']:
-                Find_links_present.Find_links_presentappend(link.attrs['href'])
+                Find_links_present.append(link.attrs['href'])
     for link in Find_links_present:
         print(link, file=open('sociallinks.txt', 'a'))
-    os.system("cat sociallinks.txt| sudo httpx -silent -status-code -title -follow-redirects -content-length")
+    os.system("cat sociallinks.txt| httpx -silent -status-code -title -follow-redirects -content-length")
  
-if __name__== '__main__':
-    banner = """
+if __name__ == '__main__':
+    try:
+        banner = """
 ██████╗ ██████╗  ██████╗ ██╗  ██╗███████╗███╗   ██╗    ██╗     ██╗███╗   ██╗██╗  ██╗    ██╗  ██╗██╗     ██╗ █████╗  ██████╗██╗  ██╗
 ██╔══██╗██╔══██╗██╔═══██╗██║ ██╔╝██╔════╝████╗  ██║    ██║     ██║████╗  ██║██║ ██╔╝    ██║  ██║██║     ██║██╔══██╗██╔════╝██║ ██╔╝
 ██████╔╝██████╔╝██║   ██║█████╔╝ █████╗  ██╔██╗ ██║    ██║     ██║██╔██╗ ██║█████╔╝     ███████║██║     ██║███████║██║     █████╔╝ 
@@ -22,16 +23,13 @@ if __name__== '__main__':
 ██████╔╝██║  ██║╚██████╔╝██║  ██╗███████╗██║ ╚████║    ███████╗██║██║ ╚████║██║  ██╗    ██║  ██║██║╚█████╔╝██║  ██║╚██████╗██║  ██╗
 ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝    ╚══════╝╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝    ╚═╝  ╚═╝╚═╝ ╚════╝ ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝
  """
-    try:
         url = input("Enter your URL: ")
         r = requests.get(url)
         Find_links = ['twitter.com', 'facebook.com', 'instagram.com', 'github.com']
         Find_links_present = []
- 
         soup = BeautifulSoup(r.content, 'html5lib')
         all_links = soup.find_all('a', href = True)
         print(banner)
         brokenLinkHijack(Find_links, all_links, Find_links_present)
     except:
         pass
-        
